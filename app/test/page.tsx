a2879d01de25cbfc2ffe514ecc8c51372277f6a5
Ruciloss <rucilos96@gmail.com>
2025-05-10 15:31:00 +0200

    update theme toggler width

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Test | NexteX",
+    description:
+        "A simple template for building modern websites with Next.js & StyleX.",
+};
+
+const Homepage = () => {
+    return <span>dddddddd</span>;
+};
+
+export default Homepage;
