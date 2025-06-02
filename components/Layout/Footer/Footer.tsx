3806d43175dd3ee00d1b4d70f2fda19be059b188
Ruciloss <rucilos96@gmail.com>
2025-06-02 18:58:50 +0200

    update

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -7,23 +7,15 @@ import Breadcrumbs from "@/components/Core/Breadcrumbs/Breadcrumbs";
 const Footer = () => (
     <div role="contentinfo" {...stylex.props(styles.wrapper)}>
         <div {...stylex.props(styles.item)}>
-            <Text size="small" color="secondary">
-                Created by Ruciloss &copy; {new Date().getFullYear()}
-            </Text>
+            <Text>Created by Ruciloss &copy; {new Date().getFullYear()}</Text>
         </div>
         <div {...stylex.props(styles.item)}>
             <Link href="/mit" ariaLabel="Link to License MIT">
                 License MIT
             </Link>
-            <Text size="small" color="secondary">
-                &bull;
-            </Text>
-            <Text size="small" color="secondary">
-                v1.0.0
-            </Text>
-            <Text size="small" color="secondary">
-                &bull;
-            </Text>
+            <Text>&bull;</Text>
+            <Text>v1.0.0</Text>
+            <Text>&bull;</Text>
             <Breadcrumbs />
         </div>
     </div>
